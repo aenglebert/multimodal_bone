0ff0a245039615b067053c6228b105aafff03a0a
Alexandre Englebert <alexandre.englebertdenis@gmail.com>
2023-12-15 13:57:34 +0100

    Update pooled vision text model

--- models/modeling_pooled_vision_text_dual_encoder.py
+++ models/modeling_pooled_vision_text_dual_encoder.py
@@ -12,32 +12,7 @@ from transformers import AutoModel
 
 from .configuration_pooled_vision_text_dual_encoder import PooledVisionTextDualEncoderConfig
 
-
-class SigLIPLoss(nn.Module):
-    def __init__(self, temperature: float = 10.0, bias: float = -10.0):
-        super().__init__()
-        self.temperature = nn.Parameter(torch.tensor(temperature))
-        self.bias = nn.Parameter(torch.tensor(bias))
-
-    def forward(self, similarity: torch.Tensor, targets=None) -> torch.Tensor:
-        logits = similarity * self.temperature + self.bias
-        n = len(logits)
-
-        if targets is None:
-            targets = torch.eye(n, device=logits.device)
-
-        labels = 2 * targets - 1
-        return -torch.sum(nn.functional.logsigmoid(labels * logits)) / n
-
-
-def contrastive_loss(logits: torch.Tensor) -> torch.Tensor:
-    return nn.functional.cross_entropy(logits, torch.arange(len(logits), device=logits.device))
-
-
-def clip_loss(similarity: torch.Tensor) -> torch.Tensor:
-    caption_loss = contrastive_loss(similarity)
-    image_loss = contrastive_loss(similarity.t())
-    return (caption_loss + image_loss) / 2.0
+from .utils import clip_loss, SigLIPLoss
 
 
 @dataclass
